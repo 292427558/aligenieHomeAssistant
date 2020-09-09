63109d11ae9f95c15b40c520ba83472628a631e6
292427558 <42735018+292427558@users.noreply.github.com>
2020-09-09 15:56:25 +0800

    单参数转化

--- src/main/java/com/junge/aligenie/service/impl/DeviceTypeOperationServiceImpl.java
+++ src/main/java/com/junge/aligenie/service/impl/DeviceTypeOperationServiceImpl.java
@@ -3,6 +3,7 @@ package com.junge.aligenie.service.impl;
 import com.alibaba.fastjson.JSONObject;
 import com.junge.aligenie.bean.AliControllResult;
 import com.junge.aligenie.bean.AliRequest;
+import com.junge.aligenie.bean.HeaderBean;
 import com.junge.aligenie.entity.DeviceTypeOperation;
 import com.junge.aligenie.entity.parameter.ServiceParameter;
 import com.junge.aligenie.entity.parameter.ServiceParameterrConversion;
@@ -17,6 +18,8 @@ import org.springframework.stereotype.Service;
 
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * mode class
@@ -51,22 +54,37 @@ public class DeviceTypeOperationServiceImpl implements DeviceTypeOperationServic
             //请求地址
             String service = deviceTypeOperation.getService();
             service.replace('.','/');
+            String responseName = deviceTypeOperation.getResponseName();
             List<ServiceParameter> serviceParameters = deviceTypeOperation.getServiceParameters();
             for (ServiceParameter serviceParameter : serviceParameters) {
                 String isConversion = serviceParameter.getIsConversion();
+                String parameterName = serviceParameter.getParameterName();
                 if("2".equals(isConversion)){
                     //需要转化参数
                     List<ServiceParameterrConversion> serviceParameterrConversions = serviceParameter.getServiceParameterrConversions();
-
+                    if(serviceParameterrConversions!=null){
+                        Map<String, ServiceParameterrConversion> conversionMap = serviceParameterrConversions.stream()
+                                .collect(Collectors.toMap(ServiceParameterrConversion::getAliParameterValue, e -> e));
+                        ServiceParameterrConversion serviceParameterrConversion = conversionMap.get(value);
+                        if(serviceParameterrConversion!=null){
+                            //转化
+                            jsonObject.put(parameterName,serviceParameterrConversion.getHassParameter());
+                        }
+                    }
                 }else {
-                    //不需要转化参数
-                    String parameterName = serviceParameter.getParameterName();
+                    //不需要转化参数-----这儿有点疑问  通常为一个参数 暂时不考虑多个参数的情况  取值为tmall中的 value
+                    //String parameterName = serviceParameter.getParameterName();
                     jsonObject.put(parameterName,value);
                 }
             }
             HttpEntity<String> requestEntity = RestHomeAssistantUtils.getHttpEntity(jsonObject.toString());
             //异步执行homeassistant的接口 不然会超时
             asycRestHomeAssistant.AsycExchangeRestApi("services/"+service, HttpMethod.POST,requestEntity);
+            //返回响应结果
+            AliControllResult aliControllResult = new AliControllResult();
+            aliControllResult.setHeader(new HeaderBean("AliGenie.Iot.Device.Control",responseName,aliRequest.getHeader().getMessageId(),1));
+            aliControllResult.getPayload().setDeviceId(deviceId);
+            return aliControllResult;
         }
         return null;
     }
